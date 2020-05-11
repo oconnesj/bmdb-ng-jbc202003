d4e9b3fc07f7d4a060efbdf9afb91d5ba91ffee0
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-05-11 15:23:59 -0400

    commit

---
+++ src/app/model/movie.class.ts
@@ -0,0 +1,16 @@
+export class Movie {
+    id: number;
+    title: string;
+    rating: string;
+    year: number;
+    director: string;
+
+
+
+
+
+constructor (number = 0, title: string = "",
+    rating: string = "", year: number = 0,
+    director: string = "") {
+}
+}
